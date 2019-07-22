86c4bf5b816f921d1ac9fb41bc65954ea667df28
PayDevD <mrpaydfordev@gmail.com>
2019-07-22 18:26:05 +0900

    init

---
+++ src/main/java/com/etrianfallout/minimapreplay/MinimapReplay/MinimapReplayApplication.java
@@ -0,0 +1,13 @@
+package com.etrianfallout.minimapreplay.MinimapReplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimapReplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimapReplayApplication.class, args);
+	}
+
+}
