b099938e42b4fdf0f8a73ff6b7162fefe64f90f0
PayDevD <mrpaydfordev@gmail.com>
2019-07-23 17:00:24 +0900

    add domain/RiotMatchTimeline

--- src/main/java/com/etrianfallout/minimapreplay/MinimapReplay/controller/RiotController.java
+++ src/main/java/com/etrianfallout/minimapreplay/MinimapReplay/controller/RiotController.java
@@ -1,6 +1,7 @@
 package com.etrianfallout.minimapreplay.MinimapReplay.controller;
 
 import com.etrianfallout.minimapreplay.MinimapReplay.domain.RiotMatch;
+import com.etrianfallout.minimapreplay.MinimapReplay.domain.RiotMatchTimeline;
 import com.etrianfallout.minimapreplay.MinimapReplay.domain.RiotMatchlist;
 import com.etrianfallout.minimapreplay.MinimapReplay.service.RiotService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,10 @@ public class RiotController {
     @GetMapping("/lol/match/matches/{matchId}")
     public RiotMatch getMatchByMatchId(@PathVariable String matchId) {
         return riotService.getMatchByMatchId(matchId);
+    }
+
+    @GetMapping("/lol/match/timlines/{matchId}")
+    public RiotMatchTimeline getTimelineByMatchId(@PathVariable String matchId) {
+        return riotService.getTimelineByMatchid(matchId);
     }
 }
