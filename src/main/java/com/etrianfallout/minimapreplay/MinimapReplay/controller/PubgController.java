34d321bf7b76ec5cc4f8cfaec488f5be984e2bfc
PayDevD <mrpaydfordev@gmail.com>
2019-07-27 23:02:16 +0900

    add crossorigin annotaion to all controller

--- src/main/java/com/etrianfallout/minimapreplay/MinimapReplay/controller/PubgController.java
+++ src/main/java/com/etrianfallout/minimapreplay/MinimapReplay/controller/PubgController.java
@@ -4,10 +4,7 @@ import com.etrianfallout.minimapreplay.MinimapReplay.domain.PubgMatch;
 import com.etrianfallout.minimapreplay.MinimapReplay.domain.PubgMatchReference;
 import com.etrianfallout.minimapreplay.MinimapReplay.service.PubgService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +13,13 @@ public class PubgController {
     @Autowired
     private PubgService pubgService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/pubg/players/{playerName}")
     public List<PubgMatch.Match> getMatchByName(@PathVariable String playerName, @RequestParam("platform") String platform) {
         return pubgService.getMatchByName(playerName, platform);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/pubg/matches/{matchId}")
     public PubgMatchReference getMatchInfo(@PathVariable String matchId, @RequestParam("platform") String platform) {
         return pubgService.getMatchInfo(matchId, platform);
